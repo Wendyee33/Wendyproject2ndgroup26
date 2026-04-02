40f32d9840103404804856de0b2479a19a769d5c
Wendy <hlatshwayowendy04@gmail.com>
2026-04-02 18:56:52 +0200

    added take screenshot

--- src/test/java/Utils/ReadFromFile.java
+++ src/test/java/Utils/ReadFromFile.java
@@ -1,30 +1,52 @@
-//package utils;
-//
-//import javax.xml.crypto.Data;
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//
-//public class ReadFromFile {
-//
-//    private static String test
-//    Data=System.getProperty("user.dir")+"/src/test/java/Testdata.testData";
-//    public static DataFormatter dataFormatter = new DataFormatter();
-//
-//    static FileInputStream fs;
-//
-//    static {
-//        try {
-//            fs = new FileInputStream(testData)
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e)
-//        }
+package Utils;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFromFile {
+
+    private static String testDataDir = System.getProperty("user.dir") + "/src/test/java/TestData/data.xlsx";
+
+    static FileInputStream fis;
+
+    static {
+        try {
+            fis = new FileInputStream(testDataDir);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static XSSFWorkbook workbook;
+
+    static {
+        try {
+            workbook = new XSSFWorkbook(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static XSSFSheet loginTab=workbook.getSheet("Login Details");
+
+    public static String username=loginTab.getRow(1).getCell(0).getStringCellValue();
+    public static String password=loginTab.getRow(1).getCell(1).getStringCellValue();
+
+
+//    @Test
+//    public void test(){
+//        System.out.println(username);
+//        System.out.println(password);
 //    }
-//
-//    static XSSFSheet workbook = new 'XSSFSheetworkbook'(fs);
-//
-//    static XSSFSheet sheet = ReadFromFile.workbook.getSheet(),
-//    public static String email = sheet.getRow(1).getCell(0).getStringCellValue();
-//
-//    public static String password = sheet.getRow(1).getCell(1).getStringCellValue();
-//
-//}
+
+
+
+
+
+
+}
